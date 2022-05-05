967b2f8835d8f77e23aa8d26ddb827f370147244
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-05-05 13:48:41 -0400

    Iniciado a criação do componete widget

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,6 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">
-  Hello world!
-</h1> 
+import { Widget } from "./components/Widget";
+
+export function App() {
+  return <Widget /> 
 }
 
-export default App
