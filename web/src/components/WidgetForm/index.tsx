5ea7331009e4df6b14412c5cde8e04a5acd3d5ac
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-05-07 16:30:16 -0400

    Adicionado form em content para comentarios de feedback

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -52,13 +52,21 @@ Isso nos dará acesso as propriedades title, image, source e alt
 export function WidgetForm() {
 
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+  
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             
       { !feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <FeedbackContentStep />
+        <FeedbackContentStep 
+          feedbackType={feedbackType} 
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
       )}
       
 
